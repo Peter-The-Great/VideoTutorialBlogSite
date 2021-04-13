a36acb1d2a82bfc8c0b7cdb506fc9e986003971d
Peter_The_Great <PjotrW15@Gmail.com>
2021-04-13 10:20:26 +0200

    Hallo

--- documenten/db_84669.sql
+++ documenten/db_84669.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 06 apr 2021 om 18:03
--- Serverversie: 10.1.47-MariaDB-0ubuntu0.18.04.1
--- PHP-versie: 7.3.27
+-- Generation Time: Apr 13, 2021 at 08:08 AM
+-- Server version: 10.1.47-MariaDB-0ubuntu0.18.04.1
+-- PHP Version: 7.3.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `admin`
+-- Table structure for table `admin`
 --
 
 CREATE TABLE `admin` (
@@ -38,16 +38,16 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Gegevens worden geëxporteerd voor tabel `admin`
+-- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`ID`, `username`, `password`, `email`, `realname`, `profile`) VALUES
-('52086616-c85c-4363-98f0-4dcd698ec356', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Mathijs54@Gmail.com', 'Mathijs Clasener', 'uploads/profile/logo.png');
+('52086616-c85c-4363-98f0-4dcd698ec356', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Mathijs54@Gmail.com', 'Mathijs Clasener', 'uploads/60740afe5996b_60740afe59975.png');
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `cat`
+-- Table structure for table `cat`
 --
 
 CREATE TABLE `cat` (
@@ -58,7 +58,7 @@ CREATE TABLE `cat` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Gegevens worden geëxporteerd voor tabel `cat`
+-- Dumping data for table `cat`
 --
 
 INSERT INTO `cat` (`ID`, `name`, `subtext`, `headimage`) VALUES
@@ -75,7 +75,7 @@ INSERT INTO `cat` (`ID`, `name`, `subtext`, `headimage`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `info`
+-- Table structure for table `info`
 --
 
 CREATE TABLE `info` (
@@ -84,16 +84,16 @@ CREATE TABLE `info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Gegevens worden geëxporteerd voor tabel `info`
+-- Dumping data for table `info`
 --
 
 INSERT INTO `info` (`id`, `text`) VALUES
-(1, '<p>Mijn naam is Niels Segaar en ik ben student bij het Grafisch Lyceum Rotterdam en ik doen de opleiding Mediatechnologie. Pellentesque sit amet orci arcu. Donec maximus lacus nunc, sed sagittis arcu tristique non. Cras pulvinar, libero non mollis malesuada, tellus magna cursus leo, commodo consequat ipsum lacus vel neque. Morbi sagittis congue ante vel aliquam. Hallo ik ben eeen persoon</p>\r\n<p><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://upload.wikimedia.org/wikipedia/commons/2/2f/Glr.jpg\" alt=\"Grafisch Lyceum Rotterdam\" width=\"232\" height=\"215\" /></p>\r\n<p>Morbi finibus elit justo, vel gravida lectus elementum ullamcorper. Quisque dapibus sollicitudin tincidunt. Nam rutrum sem sed arcu tempus sodales. Suspendisse ullamcorper eget mi quis lobortis. Donec condimentum aliquam ipsum, quis accumsan massa placerat in. Suspendisse libero nulla, accumsan sed quam eget, maximus iaculis erat. erat erat. Take me to your x-box</p>');
+(1, '<p>Mijn naam is Matthijs Clasener en ik ben leraar bij het Grafisch Lyceum Rotterdam en ik maak tutorial videos die ik op deze website wil tentoonstellen. Pellentesque sit amet orci arcu. Donec maximus lacus nunc, sed sagittis arcu tristique non. Cras pulvinar, libero non mollis malesuada, tellus magna cursus leo, commodo consequat ipsum lacus vel neque. Morbi sagittis congue ante vel aliquam. Hallo ik ben een persoon</p>\r\n<p><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://84669.ict-lab.nl/Beroeps/VideoTutorialBlogSite/uploads/simg/logo.png\" alt=\"\" width=\"177\" height=\"177\" /></p>\r\n<p>Morbi finibus elit justo, vel gravida lectus elementum ullamcorper. Quisque dapibus sollicitudin tincidunt. Nam rutrum sem sed arcu tempus sodales. Suspendisse ullamcorper eget mi quis lobortis. Donec condimentum aliquam ipsum, quis accumsan massa placerat in. Suspendisse libero nulla, accumsan sed quam eget, maximus iaculis erat. erat erat. Take me to your x-box</p>');
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `subject`
+-- Table structure for table `subject`
 --
 
 CREATE TABLE `subject` (
@@ -109,47 +109,50 @@ CREATE TABLE `subject` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Gegevens worden geëxporteerd voor tabel `subject`
+-- Dumping data for table `subject`
 --
 
 INSERT INTO `subject` (`id`, `titel`, `text`, `subtext`, `video`, `leerlijn`, `date`, `uitgelicht`, `image`) VALUES
-('02b95889-5448-47e4-8ee1-bff1d5574483', 'PHP is niet PHP', '<p>In dit document ga ik het hebben over halfmoon, wat is halfmoon en waarom is het beter om te gebruiken dan bootstrap. Dat zal ik zelf hier vertelen.</p>\r\n<p><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://i0.wp.com/css-tricks.com/wp-content/uploads/2020/07/halfmoon.png?fit=1200%2C600&amp;ssl=1\" alt=\"\" width=\"598\" height=\"299\" /></p>\r\n<p>&nbsp;</p>', '<p>Een bootstrap framework met built-in night mode</p>', 'OK_JCtrrv-c', 'PHP', '2021-03-29 12:04:28', 1, 'uploads/what-is-php-3-1.png'),
-('64f8385d-958a-494a-b916-e0a62027ce67', 'PHP is niet PHP', '<p>In dit document ga ik het hebben over halfmoon, wat is halfmoon en waarom is het beter om te gebruiken dan bootstrap. Dat zal ik zelf hier vertelen.</p>\r\n<p><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://i0.wp.com/css-tricks.com/wp-content/uploads/2020/07/halfmoon.png?fit=1200%2C600&amp;ssl=1\" alt=\"\" width=\"598\" height=\"299\" /></p>\r\n<p>&nbsp;</p>', '<p>Een bootstrap framework met built-in night mode</p>', 'OK_JCtrrv-c', 'PHP', '2021-03-29 12:04:28', 1, 'uploads/PHP1.jpg');
+('02b95889-5448-47e4-8ee1-bff1d5574483', 'PHP is niet PHP', '<p>In dit document ga ik het hebben over halfmoon, wat is halfmoon en waarom is het beter om te gebruiken dan bootstrap. Dat zal ik zelf hier vertelen.</p>\r\n<p><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://i0.wp.com/css-tricks.com/wp-content/uploads/2020/07/halfmoon.png?fit=1200%2C600&amp;ssl=1\" alt=\"\" width=\"598\" height=\"299\" /></p>\r\n<p>&nbsp;</p>', '<p>Een bootstrap framework met built-in night mode</p>', 'OK_JCtrrv-c', '2', '2021-03-29 12:04:28', 1, 'uploads/what-is-php-3-1.png'),
+('5b59a25d-4ec6-4fc2-8e9a-d246100d69cf', 'Cool', '<p>Top gear</p>', '<p>Top gear</p>', 'cDoRmT0iRic', '6', '2021-04-12 13:08:35', 0, 'uploads/60742a3326478_60742a332647d.jpg'),
+('64f8385d-958a-494a-b916-e0a62027ce67', 'PHP is niet PHP', '<p>In dit document ga ik het hebben over halfmoon, wat is halfmoon en waarom is het beter om te gebruiken dan bootstrap. Dat zal ik zelf hier vertelen.</p>\r\n<p><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://i0.wp.com/css-tricks.com/wp-content/uploads/2020/07/halfmoon.png?fit=1200%2C600&amp;ssl=1\" alt=\"\" width=\"598\" height=\"299\" /></p>\r\n<p>&nbsp;</p>', '<p>Een bootstrap framework met built-in night mode</p>', 'OK_JCtrrv-c', '2', '2021-03-29 12:04:28', 1, 'uploads/PHP1.jpg'),
+('cac348f9-4492-46ec-9a22-c492d7d97e98', 'Top gear', '<p>Gearsssssssssssss</p>', '<p>Top gearssssssssssssss</p>', 'cDoRmT0iRic', '6', '2021-04-12 13:27:09', 0, 'uploads/607430e29dc9c_607430e29dca0.jpg'),
+('d3ba4ea8-ddaf-4b71-beb5-281bf483b79f', 'Spongebob', '<p>Little Fuck that he is.</p>', '<p>Spongebob SquarePants</p>', 'S-xQXXupMlo', '1', '2021-04-13 09:56:05', 0, 'uploads/60754e954f44c_60754e954f44e.jpeg');
 
 --
--- Indexen voor geëxporteerde tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indexen voor tabel `admin`
+-- Indexes for table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexen voor tabel `cat`
+-- Indexes for table `cat`
 --
 ALTER TABLE `cat`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexen voor tabel `info`
+-- Indexes for table `info`
 --
 ALTER TABLE `info`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexen voor tabel `subject`
+-- Indexes for table `subject`
 --
 ALTER TABLE `subject`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT voor geëxporteerde tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT voor een tabel `cat`
+-- AUTO_INCREMENT for table `cat`
 --
 ALTER TABLE `cat`
   MODIFY `ID` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
