2de302ae09e7d5ba0928c37ff1cd83578972d8f0
Peter-The-Great <PjotrW15@Gmail.com>
2021-07-09 15:40:16 +0200

    Remove sensitive subjects

--- php/login/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-//typical logout system or way of destroying the session completly.
-    session_start();
-    session_unset();
-    session_destroy();
-    header("Location: ../../index.php");
-?>
