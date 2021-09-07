3efc7571ef93438aaa20f72f9cb01a0aa9759472
Peter-The-Great <56716150+Peter-The-Great@users.noreply.github.com>
2021-09-07 12:26:39 +0200

    oops

--- js/info.js
+++ js/info.js
@@ -1,4 +1,4 @@
 //get a new date
 var date = new Date();
-//Get Copyright just for the websites footer
+//Get Copyright just for the website footer
 document.getElementById("copy").innerHTML =  "Copyright © " + date.getFullYear() + " ";
